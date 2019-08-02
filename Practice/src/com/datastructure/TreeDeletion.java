d2fb740b9b9b9d0122884386ac96d464aa0cee20
selvaganapathi.g <selvaganapathi.g@IMPC1952.htcindia.local>
2019-08-02 19:22:30 +0530

    code

--- Practice/src/com/datastructure/TreeDeletion.java
+++ Practice/src/com/datastructure/TreeDeletion.java
@@ -7,43 +7,43 @@ public class TreeDeletion {
 
 	private void delete(int n) {
 
-		rearrange(root, n);
+		Node nodes = rearrange(root, n);
+		printTree(nodes);
 
 	}
 
 	private Node rearrange(Node root2, int n) {
 		if (root2 == null) {
-			return root2 ;
+			return root2;
 		}
 
 		if (root2.data > n) {
-			rearrange(root2.left, n);
-		}
-		else if (root2.data < n) {
-			rearrange(root2.right, n);
-		}
-		else if(root2.data==n) {
-			if(root2.right==null) {
-				return root2.left;
-			}
-			if(root2.left==null) {
+			 root2.left = 	rearrange(root2.left, n);
+		} else if (root2.data < n) {
+			 root2.right =rearrange(root2.right, n);
+		} else {
+			if (root2.left == null) {
 				return root2.right;
 			}
-			
-			
-			minValue(root2.right);
+			if (root2.right == null) {
+				return root2.left;
+			}
+
+			root2.data = minValue(root2.right);
+			root2.right=rearrange(root2.right, root2.data);
 		}
 		return root2;
 
 	}
 
-	private void minValue(Node root2) {
-		int min=root2.data;
-		while(root2.left!=null) {
-			min=root2.left.data;
-			
+	private int minValue(Node root2) {
+		int min = root2.data;
+		while (root2.left != null) {
+			min = root2.left.data;
+			root2 = root2.left;
 		}
-		
+		return min;
+
 	}
 
 	public static void main(String[] args) {
@@ -54,12 +54,28 @@ public class TreeDeletion {
 		td.root.right.right = new Node(85);
 		td.root.left = new Node(40);
 		td.root.left.right = new Node(45);
+		td.root.left.right.left = new Node(32);
+		td.root.left.right.right = new Node(48);
 		td.root.left.left = new Node(35);
 		td.root.left.left.right = new Node(47);
 		td.root.left.left.left = new Node(30);
 
 		td.delete(40);
+		// td.inorder();
 
+	}
+
+	private void inorder() {
+		printTree(root);
+	}
+
+	private void printTree(Node root2) {
+		if (root2 == null)
+			return;
+
+		printTree(root2.left);
+		System.out.print(root2.data + " ");
+		printTree(root2.right);
 	}
 
 }
